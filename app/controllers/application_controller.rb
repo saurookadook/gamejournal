1daa2b9993d4d158535bf5dcf777c79ef059fe60
saurookadook <maskiella@gmail.com>
2019-01-03 14:45:59 -0500

    Add utility method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,21 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # For Devise
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+
+  # Controller Utilities
+  def set_user!
+    if params[:id]
+      @user = User.find(params[:id])
+    elsif params[:user_id]
+      @user = User.find(params[:user_id])
+    end
+  end
+  
 end

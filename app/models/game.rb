b5f91808be9665e65255d8fada2b52ba84f0fef2
saurookadook <maskiella@gmail.com>
2019-01-22 08:31:45 -0500

    Initial syling fixes

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,12 +7,12 @@ class Game < ApplicationRecord
   has_many :game_platforms
   has_many :platforms, through: :game_platforms
 
+  validates :title, :publisher, presence: true
+
   # Custom methods
   def display_attributes
     { 
-      Title: self.title,
       Platforms: self.display_platforms,
-      Publisher: self.publisher,
       Summary: self.summary
     }
   end
